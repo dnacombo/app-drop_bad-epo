5e1c251fc210a6220ac06437c51703029ae2928f
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2024-03-07 18:09:31 +0100

    add a hack to use list of trials to drop in a fake events.tsv file

--- main.py
+++ main.py
@@ -7,19 +7,35 @@ import os
 import json
 import numpy as np
 import mne
-#import helper
+import helper
 import re
+
 
 # load inputs from config.json
 with open('config.json') as config_json:
-	#config =  helper.convert_parameters_to_None(json.load(config_json))
-	config = json.load(config_json)
+	config =  helper.convert_parameters_to_None(json.load(config_json))
 
 data_file = config['mne']
 
 epochs = mne.read_epochs(data_file,verbose=False)
-drop = config['drop'].split(',')
-drop = [int(x) for x in drop]
-epochs.drop(drop)
 
-epochs.save(os.path.join('out_dir','meg-epo.fif'))
+# read config['events']
+with open(config['events']) as f:
+	todrop1 = f.read()
+# turn todrop1 into a list of strings and remove leading and trailing whitespace or commas
+todrop1 = todrop1.split(',')
+# turn it to integers
+# remove leading and trailing whitespace or commas in the list
+todrop1 = [re.sub(r'^\s*|\s*$', '', x) for x in todrop1]
+# remove empty strings
+todrop1 = list(filter(None, todrop1))
+todrop1 = [int(x) for x in todrop1]
+
+todrop2 = config['drop'].split(',')
+todrop2 = [int(x) for x in todrop2]
+
+# create union of todrop1 and todrop2
+todrop = list(set(todrop1) | set(todrop2))
+
+epochs.drop(todrop)
+epochs.save(os.path.join('out_dir','meg.fif'))
